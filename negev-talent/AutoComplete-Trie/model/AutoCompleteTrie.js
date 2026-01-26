279321c7a20879cfd6bf28fa50014303a30def0c
hamedibnatmi <hamed2599@gmail.com>
2026-01-26 14:38:23 +0200

    Add predictWords function

--- negev-talent/AutoComplete-Trie/model/AutoCompleteTrie.js
+++ negev-talent/AutoComplete-Trie/model/AutoCompleteTrie.js
@@ -9,19 +9,44 @@ class AutoCompleteTrie {
         let lword = word.toLocaleLowerCase();
         for (let char of lword) {
             if (!node.children[char]) {
-                node.value = char;
                 node.children[char] = new AutoCompleteTrieNode();
+                node.children[char].value = char;
             }
-            console.log("node", node)
             node = node.children[char];
         }
         node.endOfWord = true;
+        console.log("Root: ", this.root)
+    }
+
+    predictWords(prefix) {
+        let node = this.root;
+        let lPrefix = prefix.toLocaleLowerCase();
+
+        for (let char of lPrefix) {
+            if (char !== node.children[char].value) return []; // if a char is not match
+            node = node.children[char]; // if yes node procced
+        }
+        return this.collectWords(node, lPrefix)
     }
 
 
 
+    collectWords(node, lPrefix) {
+        let wordsCollection = [];
+
+        if (node.endOfWord) {
+            wordsCollection.push(lPrefix);
+        }
+        for (let child of Object.values(node.children)) {
+
+            wordsCollection.push(...this.collectWords(child, lPrefix + child.value))
+        }
+        return wordsCollection;
+    }
 }
 
 
 let t = new AutoCompleteTrie();
-console.log("t", t.addWord("Hamed"))
+t.addWord("Hammed");
+t.addWord("Hammie");
+console.log(t.predictWords("Ha"))
