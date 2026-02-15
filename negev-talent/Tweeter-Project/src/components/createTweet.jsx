5765ae438d296442d761fd0a36ac2bce838cf5a4
hamedibnatmi <hamed2599@gmail.com>
2026-02-15 09:55:16 +0200

    Added Create tweet component logic

--- negev-talent/Tweeter-Project/src/components/createTweet.jsx
+++ negev-talent/Tweeter-Project/src/components/createTweet.jsx
@@ -1,19 +1,21 @@
-
+import { useTextAreaContext } from "../contexts/textAreaContext";
 
 const CreatTweet = () => {
-
-
+    const { textAreaInput, setTextAreaInput, tweetsList, setTweetsList } = useTextAreaContext();
+    const submitHandle = (e) => {
+        e.preventDefault();
+        setTweetsList([textAreaInput, ...tweetsList])
+    }
     return (
         <>
             <div className="create-tweet-form">
-                <form action="">
-                    <textarea className="input" type="text" placeholder="What you have in mind..." />
+                <form action="" onSubmit={submitHandle}>
+                    <textarea className="input" type="text" placeholder="What you have in mind..." onChange={e => setTextAreaInput(e.target.value)} />
                     <input className="tweet-btn" type="submit" value={"Tweet"} />
                 </form>
             </div>
         </>
     )
 }
-
 
 export default CreatTweet;
